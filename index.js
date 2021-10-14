470882376ff4f64253c367deaaa68fa6fb4409ea
timofey fayzullin <89485758+dartik24@users.noreply.github.com>
2021-10-14 11:53:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ excerpted for educational purposes with credit to the author.
 update by TF on October 4th, 2021 at 8:14 PM*/
 
 function makeAchart(){
+//taking in values from input table and converting them into vars
   var fr = parseInt(document.getElementById('botr').value);
   var sr = parseInt(document.getElementById('topr').value);
   var fc = parseInt(document.getElementById('botc').value);
   var sc = parseInt(document.getElementById('topc').value);
+//setting up table and error fields for the information to be inputed in
   var table = document.getElementById('tbl');
   var err = document.getElementById('errors');
+//setting up temporary output and error fields
   var output="";
   var errid="";
+//linking 
   err.innerHTML=errid;
-  table.innerHTML=output
+  table.innerHTML=output;
+//check for the table to be made no matter which input comes first
   if (fr > sr)
   {
     var t;
@@ -32,12 +37,14 @@ function makeAchart(){
     fc = sc;
     sc = t;
   }
-  if(fc > 250 || sr > 250 || fr > 250 || sc > 250 || fc < -250 || sc < -250 || fr < -250 || sr < -250 || isNaN(fc) || isNaN(fr) || isNaN(sc) || isNaN(sr))
+//check for values which should not be inputed as well as error message set up
+  if(fc > 100 || sr > 100 || fr > 100 || sc > 100 || fc < -100 || sc < -100 || fr < -100 || sr < -100 || isNaN(fc) || isNaN(fr) || isNaN(sc) || isNaN(sr))
   {
-    errid += "Please enter a valid number range between -250 and 250";
+    errid += "Please enter a valid number range between -100 and 100";
     err.innerHTML=errid;
     return;
   }
+//table generator
   for(var i=fr - 1; i<=sr; i++){
     output+="<tr>";
     for(var j=fc - 1; j<=sc; j++){
